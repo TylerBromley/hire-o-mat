09458a2993aad75e4ceed69b43c78f0e187aa1fb
Tyler Bromley <thelony@gmail.com>
2019-03-21 10:53:05 -0700

    made the profile filter multiple choice

--- hire_o_mat/hire_app/filters.py
+++ hire_o_mat/hire_app/filters.py
@@ -1,8 +1,13 @@
 import django_filters
-
-from .models import UserProfile
+from django import forms
+from .models import City, Skill, UserProfile
 
 class UserProfileFilter(django_filters.FilterSet):
+    city = django_filters.ModelMultipleChoiceFilter(queryset=City.objects.all(),
+        widget=forms.CheckboxSelectMultiple)
+    skills = django_filters.ModelMultipleChoiceFilter(queryset=Skill.objects.all(),
+        widget=forms.CheckboxSelectMultiple)
+
     class Meta:
         model = UserProfile
         fields = ['city', 'skills', ]
